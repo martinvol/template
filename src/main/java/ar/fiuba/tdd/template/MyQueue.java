bb0091d4aca45be80c835c8676093f6b9a19b121
Martín Volpe <martin.volpe@gmail.com>
2016-08-28 18:50:32 -0300

    Primer test funcionando

---
+++ src/main/java/ar/fiuba/tdd/template/MyQueue.java
@@ -0,0 +1,32 @@
+package ar.fiuba.tdd.template;
+
+/**
+ * Created by martin on 8/28/16.
+ */
+public class MyQueue<T> implements Queue{
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public Object top() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
--- src/test/java/ar/fiuba/tdd/template/MainTests.java
+++ src/test/java/ar/fiuba/tdd/template/MainTests.java
@@ -1,13 +1,21 @@
 package ar.fiuba.tdd.template;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainTests {
 
+    MyQueue queue;
+
+    @Before
+    public void setUp() {
+        queue = new MyQueue<Integer>();
+    }
+
     @Test
     public void dummy() {
-        assertEquals(0, 0);
+        assertEquals(queue.size(), 0);
     }
 }
